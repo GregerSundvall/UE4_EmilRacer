f2d7c7bbcb45a0fc07340d92b542a0da0283de7f
Greger Sundvall <gsundvall@gmail.com>
2022-04-12 20:38:55 +0200

    end of 12/04

---
+++ Source/EmilRacer/UI/RaceOverlayWidget.cpp
@@ -0,0 +1,17 @@
+﻿
+#include "RaceOverlayWidget.h"
+
+void URaceOverlayWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	AddPlayerStatus(0);
+
+	UE_LOG(LogTemp, Warning, TEXT("URaceOverlayWidget::NativeConstruct"))
+}
+
+void URaceOverlayWidget::AddPlayerStatus(int PlayerIndex)
+{
+	UUserWidget* PlayerStatus = NewObject<UUserWidget>(this, PlayerWidgetClass);
+	PlayerStatus->AddToViewport();
+	
+}
---
+++ Source/EmilRacer/UI/RaceOverlayWidget.h
@@ -0,0 +1,21 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RaceOverlayWidget.generated.h"
+
+
+UCLASS()
+class EMILRACER_API URaceOverlayWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	void NativeConstruct() override;
+	
+	void AddPlayerStatus(int PlayerIndex);
+
+	UPROPERTY(EditAnywhere, Category = RaceOverlay)
+	TSubclassOf<UUserWidget> PlayerWidgetClass;
+	
+	
+};
